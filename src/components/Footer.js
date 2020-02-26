e4ed40173d8250439170042ece28ad965493fd9c
DeAndre Boston <dboston3@yahoo.com>
2020-02-25 19:55:39 -0600

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -48,19 +48,10 @@ const Footer = () => (
           <FooterContainer>
             <Fade left>
               <TextFooter fontSize={[2, 3]}>
-                <span>{`${name} Portfolio - Powered by `}</span>
-                <Link href="https://www.gatsbyjs.org/">Gatsby</Link>
-                <span>, </span>
-                <Link href="https://www.contentful.com/" mr={1}>
-                  Contentful
-                </Link>
-                <span> and </span>
-                <Link href="https://www.netlify.com/" mr={1}>
-                  Netlify
-                </Link>
-                <span role="img" aria-label="heart">
-                  ❤️
+                <span role="img" aria-label="copyright">
+                  © 2020
                 </span>
+                <span>{`${name} Portfolio - Powered by `}</span>
               </TextFooter>
             </Fade>
             <Flex>
